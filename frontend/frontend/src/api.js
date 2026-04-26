52ad01a68c644cdad3eee92f6e5dde05bddaf7f1
dasunithiwanshika-ship-it <dasunithiwanshikagmail.com>
2026-04-26 20:26:33 +0530

    connect backend to railway

--- frontend/frontend/src/api.js
+++ frontend/frontend/src/api.js
@@ -1,5 +1,5 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: "http://localhost:5000/api/items"
+  baseURL: "https://item-manager-labtest-production.up.railway.app/api/items"
 });
